d65e7c9516c8c05162c015ff34e3d741e9599357
Marvin Blum <marvin.blum@emvi.com>
2021-01-10 19:14:04 +0100

    Started resizing favicons using query parameter.

--- favicon/serve.go
+++ favicon/serve.go
@@ -4,12 +4,25 @@ import (
 	"github.com/pirsch-analytics/faser/db"
 	"github.com/pirsch-analytics/faser/server"
 	"net/http"
+	"path"
 	"path/filepath"
+	"strconv"
 	"time"
 )
 
+var sizes = []int{
+	16,
+	32,
+	64,
+	96,
+	128,
+	196,
+}
+
+// ServeFavicon looks up a favicon and serves the file in the desired dimensions if possible or the default icon otherwise.
 func ServeFavicon(w http.ResponseWriter, r *http.Request) {
 	hostname := getHostname(r.URL.Query().Get("url"))
+	size, _ := strconv.Atoi(r.URL.Query().Get("size"))
 
 	if hostname == "" {
 		serveDefaultFavicon(w, r)
@@ -28,9 +41,31 @@ func ServeFavicon(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.ServeFile(w, r, filepath.Join(filesDir, hostname, domain.Filename.String))
+	filename := getFilenameForSize(domain.Filename.String, size)
+	http.ServeFile(w, r, filepath.Join(filesDir, hostname, filename))
 }
 
 func serveDefaultFavicon(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, server.Config().DefaultFavicon)
+}
+
+func getFilenameForSize(filename string, size int) string {
+	if size <= 0 {
+		return filename
+	}
+
+	for _, s := range sizes {
+		if size <= s {
+			size = s
+			break
+		}
+	}
+
+	if size > sizes[len(sizes)-1] {
+		size = sizes[len(sizes)-1]
+	}
+
+	ext := path.Ext(filename)
+	filenameWithoutExt := filename[:len(filename)-len(ext)]
+	return filenameWithoutExt + "-" + strconv.Itoa(size) + ext
 }
--- favicon/serve_test.go
+++ favicon/serve_test.go
@@ -1,1 +1,17 @@
 package favicon
+
+import "testing"
+
+func TestGetFilenameForSize(t *testing.T) {
+	if o := getFilenameForSize("foo.png", 12); o != "foo-16.png" {
+		t.Fatalf("Filename with minimum size must have been returned, but was: %v", o)
+	}
+
+	if o := getFilenameForSize("foo.png", 1218); o != "foo-196.png" {
+		t.Fatalf("Filename with maximum size must have been returned, but was: %v", o)
+	}
+
+	if o := getFilenameForSize("foo.png", 64); o != "foo-64.png" {
+		t.Fatalf("Filename with exact size must have been returned, but was: %v", o)
+	}
+}
